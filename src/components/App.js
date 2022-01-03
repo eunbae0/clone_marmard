6bfa99c4d262e6e71e4b3a8a4b13b82b3d79255e
은배 <geb9211@gmail.com>
2022-01-04 04:10:43 +0900

    초기 설정

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import AppRouter from "./Router";
-
+//
 function App() {
   return (
     <AppRouter />
--- src/components/Router.js
+++ src/components/Router.js
@@ -4,12 +4,34 @@ import Footer from "./Footer";
 import Home from "../pages/Home";
 
 export default function AppRouter() {
-  return (
+  return ( 
     <>
       <BrowserRouter>
         <Navigation />
         <Routes>
-          <Route exact path="/" element={<Home />} />
+          <Route path="member/login" element={<Login />} />
+          <Route path="member/join" element={<Join />} />
+          <Route path="member/myshop" element={<MyShop />} />
+        </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="shopinfo/brandstory" element={<BrandStory />} />
+          <Route path="shopinfo/contact" element={<Contact />} />
+          <Route path="product" element={<Shop/>}>
+            <Route index element={<HandCream />} />
+            <Route path="bodywash" element={<BodyWash />}/>
+            <Route path="bodylotion" element={<BodyLotion />}/>
+            <Route path="bodyspary" element={<BodySpray />}/>
+            <Route path="handcream" element={<HandCream />}/>
+            <Route path="handwash" element={<HandWash />}/>
+            <Route path="set" element={<Set />}/>
+          </Route>
+          <Route path="board" element={<Board/>}>
+            <Route index element={<Notice />} />
+            <Route path="notice" element={<Notice />}/>
+            <Route path="review" element={<Review />}/>
+          </Route>
+          <Route path="order" element={<Order />}/>
         </Routes>
         <Footer />
       </BrowserRouter>
